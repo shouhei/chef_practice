0279741beb8a172ca45c8903bf9c1a5c906c9082
山口将平 <shouhei.yamaguci.ht@gmail.com>
2014-11-25 20:30:23 +0900

    initial commit

---
+++ site-cookbooks/iptables/recipes/default.rb
@@ -0,0 +1,21 @@
+#
+# Cookbook Name:: iptables
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+template "iptables" do
+	path "/etc/sysconfig/iptables"
+	owner "root"
+	group "root"
+	mode 0664
+  notifies :restart, 'service[iptables]'
+end
+
+service "iptables" do
+  supports :status => true, :restart => true
+  action [:enable, :start]
+end
